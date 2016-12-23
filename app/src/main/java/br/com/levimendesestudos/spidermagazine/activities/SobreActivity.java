686d46a31e782b3e841e0b554096b1111fe7d06f
429317980 <levi.silva.mendes@gmail.com>
2016-12-23 10:26:55 -0200

    Implementação da tela sobre

--- app/src/main/java/br/com/levimendesestudos/spidermagazine/activities/SobreActivity.java
+++ app/src/main/java/br/com/levimendesestudos/spidermagazine/activities/SobreActivity.java
@@ -1,20 +1,17 @@
 package br.com.levimendesestudos.spidermagazine.activities;
 
-import android.graphics.Color;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import br.com.levimendesestudos.spidermagazine.R;
-import br.com.levimendesestudos.spidermagazine.utils.ToastUtil;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableStringBuilder;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.TextView;
-
-import static br.com.levimendesestudos.spidermagazine.utils.ToastUtil.showToast;
 
 public class SobreActivity extends BaseActivity {
 
@@ -22,6 +19,7 @@ public class SobreActivity extends BaseActivity {
     Toolbar toolbar;
     @BindView(R.id.tvLinks)
     TextView tvLinks;
+    private SpannableStringBuilder spanLinks = new SpannableStringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,75 +36,74 @@ public class SobreActivity extends BaseActivity {
         links();
     }
 
+    /**
+     * Adiciona os links
+     */
     private void links() {
-        String email     = "levi.silva.mendes@gmail.com";
-        String linkedin  = "https://br.linkedin.com/in/levi-mendes-56567035";
-        String github    = "https://github.com/levi-mendes";
-        String bitbucket = "https://bitbucket.org/levi-mendes/";
-        String facebook  = "https://m.facebook.com/profile.php?id=100010499918526";
-
-        SpannableStringBuilder spanTxt = new SpannableStringBuilder();
-
-        spanTxt.append("Email:   ");
-        spanTxt.append(email);
-        spanTxt.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                showToast(getApplicationContext(), email + " Clicked");
-            }
-
-        }, spanTxt.length() - email.length(), spanTxt.length(), 0);
-        spanTxt.append("\n\n");
-
-        spanTxt.append("Linkedin:   ");
-        spanTxt.append(linkedin);
-        spanTxt.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                showToast(getApplicationContext(), linkedin + " Clicked");
-            }
-
-        }, spanTxt.length() - linkedin.length(), spanTxt.length(), 0);
-        spanTxt.append("\n\n");
-
-        //spanTxt.setSpan(new ForegroundColorSpan(Color.BLACK), 32, spanTxt.length(), 0);
-        spanTxt.append("Github:   ");
-        spanTxt.append(github);
-        spanTxt.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                showToast(getApplicationContext(), github + " Clicked");
-            }
-        }, spanTxt.length() - github.length(), spanTxt.length(), 0);
-        spanTxt.append("\n\n");
-
-        //spanTxt.setSpan(new ForegroundColorSpan(Color.BLACK), 32, spanTxt.length(), 0);
-        spanTxt.append("Bitbucket:   ");
-        spanTxt.append(bitbucket);
-        spanTxt.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                showToast(getApplicationContext(), bitbucket + " Clicked");
-            }
-        }, spanTxt.length() - bitbucket.length(), spanTxt.length(), 0);
-        spanTxt.append("\n\n");
-
-        //spanTxt.setSpan(new ForegroundColorSpan(Color.BLACK), 32, spanTxt.length(), 0);
-        spanTxt.append("Facebook:   ");
-        spanTxt.append(facebook);
-        spanTxt.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                showToast(getApplicationContext(), facebook + " Clicked");
-            }
-        }, spanTxt.length() - facebook.length(), spanTxt.length(), 0);
-        spanTxt.append("\n\n");
+        adicionarLinkEmail("Email:   ", "levi.silva.mendes@gmail.com");
+        adicionarLink("Linkedin:   ",   "https://br.linkedin.com/in/levi-mendes-56567035");
+        adicionarLink("Github:   ",     "https://github.com/levi-mendes");
+        adicionarLink("Bitbucket:   ",  "https://bitbucket.org/levi-mendes/");
+        adicionarLink("Facebook:   ",   "https://m.facebook.com/profile.php?id=100010499918526");
 
         tvLinks.setMovementMethod(LinkMovementMethod.getInstance());
-        tvLinks.setText(spanTxt, TextView.BufferType.SPANNABLE);
+        tvLinks.setText(spanLinks, TextView.BufferType.SPANNABLE);
     }
 
-    private void adicionarLink() {
+    /**
+     * Adiciona link de email
+     * @param titulo
+     * @param email
+     */
+    private void adicionarLinkEmail(String titulo, String email) {
+        spanLinks.append(titulo);
+        spanLinks.append(email);
+        spanLinks.setSpan(clickEnviarEmail(email), spanLinks.length() - email.length(), spanLinks.length(), 0);
+        spanLinks.append("\n\n");
 
+    }
+
+    private void adicionarLink(String titulo, String link) {
+        spanLinks.append(titulo);
+        spanLinks.append(link);
+        spanLinks.setSpan(abrirLink(link), spanLinks.length() - link.length(), spanLinks.length(), 0);
+        spanLinks.append("\n\n");
+    }
+
+    /**
+     * Abre link de internet
+     * @param link
+     * @return
+     */
+    private ClickableSpan abrirLink(String link) {
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(link));
+                startActivity(i);
+            }
+        };
+
+        return clickableSpan;
+    }
+
+
+    private ClickableSpan clickEnviarEmail(String email) {
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                enviarEmail(email);
+            }
+        };
+
+        return clickableSpan;
+    }
+
+    private void enviarEmail(String email) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", email, null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT,      "Assunto");
+        emailIntent.putExtra(Intent.EXTRA_TEXT,         "Corpo email");
+        startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
     }
 }
